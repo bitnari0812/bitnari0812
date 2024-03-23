da883e368cf57801802deca4379e7b9a50aabc36
오빛나리 <163955182+bitnari0812@users.noreply.github.com>
2024-03-23 17:06:02 +0900

    Application2

--- 01_JAVA practice/test/src/chap03/test/Application2.java
+++ 01_JAVA practice/test/src/chap03/test/Application2.java
@@ -15,6 +15,26 @@ public class Application2 {
         //                *   - 10과 5의 나눈 후 몫 : 2
         //                *  */
 
+        Calculator cal = new Calculator();
+
+        // 메소드 호출 확인
+        Calculator.checkmethod();
+
+        //10과 20의 합 : 30
+        int isum = cal.sumTwoNumber(10,20);
+        System.out.println("10과 20의 합은? = "+isum);
+
+        //10과 5의 차 : 5
+        int isum2 = cal.minusTwoNumber(10,5);
+        System.out.println("10과 5의 차는? = "+isum2);
+
+        //10과 5의 곱 : 50
+        int isum3 = cal.multiTwoNumber(10,5);
+        System.out.println("10과 5의 곱은? = "+isum3);
+
+        //10과 5의 나눈 후 몫 : 2
+        int isum4 = cal.divideTwoNumber(10,5);
+        System.out.println("10과 5의 나눈 후 몫은? = "+isum4);
 
     }
 }
