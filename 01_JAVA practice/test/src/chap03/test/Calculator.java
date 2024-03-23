b20d0dd1c012e7ab19202ec37703428eae83f82e
오빛나리 <163955182+bitnari0812@users.noreply.github.com>
2024-03-23 17:06:26 +0900

    Calculator

--- 01_JAVA practice/test/src/chap03/test/Calculator.java
+++ 01_JAVA practice/test/src/chap03/test/Calculator.java
@@ -1,8 +1,7 @@
 package chap03.test;
-
 public class Calculator {
     public static void main(String[] args) {
-         /* 다음 조건을 만족하는 프로그램을 작성하시오.
+        /* 다음 조건을 만족하는 프로그램을 작성하시오.
          *   구현 클래스 Calculator, Application01
          *   구현 내용
          *   Calculator
@@ -10,8 +9,35 @@ public class Calculator {
          *   - sumTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 합을 리턴한다.
          *   - minusTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 차를 리턴한다.
          *   - multiTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 곱한 값을 리턴한다.
-         *   - divideTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 나눈 값을 리턴한다.
-
-          */
+         *   - divideTwoNumber(a:int, b:int): int 설명 : 두 수를 입력 받아 나눈 값을 리턴한다
+         */
+        // 함수 호출, 확인용 메소드
     }
-}
+
+    public static void checkmethod() {
+    }
+
+    public void checkMethod() {
+            System.out.println("함수 호출 확인용 메소드입니다.");
+        }
+
+        // 두 수를 입력 받아 합을 리턴하는 메소드
+        public int sumTwoNumber(int a, int b) {
+            return a + b;
+        }
+
+        // 두 수를 입력 받아 차를 리턴하는 메소드
+        public int minusTwoNumber(int a, int b) {
+            return a - b;
+        }
+
+        // 두 수를 입력 받아 곱한 값을 리턴하는 메소드
+        public int multiTwoNumber(int a, int b) {
+            return a * b;
+        }
+
+        // 두 수를 입력 받아 나눈 값을 리턴하는 메소드
+        public int divideTwoNumber(int a, int b) {
+            return a / b;
+        }
+    }
