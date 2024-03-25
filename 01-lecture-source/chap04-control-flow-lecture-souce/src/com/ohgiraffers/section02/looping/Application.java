ebc05de1c64f2d8f21ed75297c5acc31e1aa66e8
DESKTOP-F4BC13L\hi <solitarybiz@gmail.com>
2024-03-25 17:25:50 +0900

    2024.03.25

---
+++ 01-lecture-source/chap04-control-flow-lecture-souce/src/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section02.looping;
+
+import com.ohgiraffers.section01.conditional.A_if;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+        //a.testSimpleForStatement();
+        //a.testSimpleExample1();
+        //a.testSimpleExample2();
+        //a.testForExample3();
+        //a.printSimpleGugudan();
+
+        A_nestedFor an = new A_nestedFor();
+        //an.printGugudanFromTwoToNice();
+        //an.printStarInputRowTimes();
+        an.printTriangleStars();
+
+    }
+}
