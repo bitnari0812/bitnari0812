dbda74075c7a0b94c8333182bcd5518d1a08d781
DESKTOP-F4BC13L\hi <solitarybiz@gmail.com>
2024-03-27 16:42:09 +0900

    2024.03.27

--- 01_JAVA practice/test/src/Question/Application1.java
+++ 01_JAVA practice/test/src/Question/Application1.java
@@ -3,23 +3,18 @@ package Question;
 public class Application1 {
     public static void main(String[] args) {
 
-        //1. 기본자료형 8가지를 선언 및 초기화
+        boolean isTrue = true;
+        char cha = 'a';
         byte bnum = 1;
         short snum = 2;
-        int inum = 3;
-        long lnum = 4L;
-        float fnum = 5.0f;
-        double dnum = 6.0d;
-        char cha = 'A';
-        boolean boo = false;
-        String str = "안녕하세요";
+        int inum = 14;
+        long lnum = 322444L;
+        float fnum = 2.3f;
+        double dnum = 34.32;
 
-        //2. 정수 int의 값을 long 타입과 합
-        int sum = inum+(int)lnum;
-        System.out.println("int 값과 long 값의 합은? = "+(sum));
+        int sum = inum + (int) lnum;
 
-        //3. 삼항연산자를 이용해 2.의 합계를 0보다 클 때 정수입니다, 0보다 작을 때 음수입니다 표기
-        String sum2 = (sum > 0)? "정수입니다." : "음수입니다.";
+        String str = (sum > 0)? "양수입니다." : "음수입니다.";
     }
 
 }
--- 01_JAVA practice/test/src/Question/testMethod.java
+++ 01_JAVA practice/test/src/Question/testMethod.java
@@ -1,28 +1,31 @@
 package Question;
-public class testMethod {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class TestMethod {
+    public void Calculator(int num1, int num2) {
+        System.out.println("두 수를 더한 값은 " + (num1 += num2) + "입니다.");
+        System.out.println("두 수를 뺀 값은 " + (num1 -= num2) + "입니다.");
+        System.out.println("두 수를 곱한 값은 " + (num1 *= num2) + "입니다.");
+        System.out.println("두 수를 나눈 몫은 " + (num1 /= num2) + "입니다.");
+        System.out.println("두 수를 나눈 나머지는  " + (num1 %= num2) + "입니다.");
     }
-    testMethod Calculator = new testMethod();
-    public int sumTwoNumber(int a, int b) {
-        int sum = a + b;
-        return sum;
+
+    public double CircleArea() {
+        final double pie = 3.14;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("반지름을 입력하세요 : ");
+        int r = sc.nextInt();
+
+        double result = pie * r * r;
+        return result;
     }
-    public int minusTwoNumber(int a, int b) {
-        int minus = a - b;
-        return minus;
-    }
-    public int multiTwoNumber(int a, int b) {
-        int multi = a * b;
-        return multi;
-    }
-    public int divideTwoNumber(int a, int b) {
-        int divide = a / b;
-        return divide;
-    }
-    public int percent(int a, int b){
-        int percent = a % b;
-        return percent;
+
+    public static String TestRandom() {
+        int randomNumber = (int) (Math.random() * 10) + 1;
+        String result = "생성한 난수는 " + randomNumber + "입니다.";
+        return result;
     }
 
 }
